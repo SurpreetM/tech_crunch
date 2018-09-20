0cc025e6cbfd74ae23060a7fe89ab1bb9dcf0073
surpreet.mundra <surpreet.mundra@gmail.com>
2018-09-20 18:57:45 +0000

    created cli class and added it to the environment via lib/tech_crunch.rb

--- lib/tech_crunch.rb
+++ lib/tech_crunch.rb
@@ -1,5 +1,5 @@
-require "tech_crunch/version"
-
+require_relative "./tech_crunch/version"
+require_relative "./tech_crunch/cli"
 module TechCrunch
   # Your code goes here...
 end
---
+++ lib/tech_crunch/cli.rb
@@ -0,0 +1,7 @@
+class TechCrunch::Cli 
+  
+  def test
+    puts "This is the CLI class"
+  end 
+  
+end 
