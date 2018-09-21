b9ddc5596eafce9485a96811939dd16ee84aaf04
surpreet.mundra <surpreet.mundra@gmail.com>
2018-09-21 19:56:53 +0000

    working on scraping

--- lib/tech_crunch/scraper.rb
+++ lib/tech_crunch/scraper.rb
@@ -4,10 +4,16 @@ class TechCrunch::Scraper
 
   def home_page(url)
     doc = Nokogiri::HTML(open(url))
-    container = doc.search(".river div")
-    articles = container.css(".post-block")
     
-    article_title = articles.css(".post-block__title a")[0].text.strip
+    container = doc.search(".river div")
+    articles = []
+    
+    container.each do |article|
+      article.css(".post-block") each do |element|
+        article_title = element.css(".post-block__title a").text.strip
+        articles << {title: article_title}
+      end 
+    end 
     
     binding.pry
     
