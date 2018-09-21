5111c9d45083328dbcb66454100f0467de32db92
surpreet.mundra <surpreet.mundra@gmail.com>
2018-09-21 19:27:07 +0000

    testing the scraped elements

--- lib/tech_crunch/scraper.rb
+++ lib/tech_crunch/scraper.rb
@@ -4,28 +4,31 @@ class TechCrunch::Scraper
 
   def home_page(url)
     doc = Nokogiri::HTML(open(url))
-    container = doc.search(".river").children
+    container = doc.search(".river div")
+    
+    binding.pry
+    
+  end
     
      # start by creating an empty array to push the articles into
-    articles = []
+     # articles = []
     
     # iterate through each article in the container to create an array of articles. Each article needs to be a hash with the above key values 
     
-     container.each do |article|
-       article.each do |element|
-        article_title = container.css(".post-block__title a").text.strip
-        author = container.css(".river-byline__authors a").text.strip
-        time_published = container.css(".river-byline time").attribute("datetime").value
-        articles << {title: article_title, time_published: time_published, author: author}
-      end 
-    end
+     #container.each do |article|
+      # article.each do |element|
+       # article_title = container.css(".post-block__title a").text.strip
+        #author = container.css(".river-byline__authors a").text.strip
+        #time_published = container.css(".river-byline time").attribute("datetime").value
+        #articles << {title: article_title, time_published: time_published, author: author}
+      #end 
+    #end
     #return the articles Array
     
     #articles
   
-  end
-  binding.pry
   
+
 
    
     
