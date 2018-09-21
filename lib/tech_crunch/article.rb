e79bf52730d6b4d81e0c73a5bec0048b9071a222
surpreet.mundra <surpreet.mundra@gmail.com>
2018-09-21 15:20:38 +0000

    created methods for the article class

--- lib/tech_crunch/article.rb
+++ lib/tech_crunch/article.rb
@@ -1,6 +1,30 @@
 require 'pry'
 class TechCrunch::Article 
   
+  attr_accessor :title, :time_published, :author
+  
+  @@all = []
+  
+  def self.create_articles_from_homepage(articles_array)
+    articles_array. each do |article_hash|
+      Article.new(article_hash)
+    end 
+  end 
+  
+  def initialize(article_hash)
+    article_hash.each do |attribute, value|
+      self.send("#{attribute}=", value)
+    end
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+    
+    
+  end 
+  
   
   
   
