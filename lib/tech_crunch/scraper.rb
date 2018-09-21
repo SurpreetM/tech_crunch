b41403d7f47441aa46e256c766b74cfa3be9663c
surpreet.mundra <surpreet.mundra@gmail.com>
2018-09-21 19:02:16 +0000

    attempting to fix the enumerative in the scraper class

--- lib/tech_crunch/scraper.rb
+++ lib/tech_crunch/scraper.rb
@@ -6,23 +6,30 @@ class TechCrunch::Scraper
     doc = Nokogiri::HTML(open(url))
     container = doc.search(".river").children
     
-    # iterating through each article in the container to create an array of articles. Each article needs to be a hash with the above key values 
-    
-    #start by creating an empty array to push the articles into
+     # start by creating an empty array to push the articles into
     articles = []
     
-    container.each do |article|
-      article_title = container.css(".post-block__title a").text.strip
-      author = container.css(".river-byline__authors a").text.strip
-      time_published = container.css(".river-byline time").attribute("datetime").value
-      articles << {title: article_title, time_published: time_published, author: author}
-      binding.pry
+    # iterate through each article in the container to create an array of articles. Each article needs to be a hash with the above key values 
+    
+     container.each do |article|
+       article.each do |element|
+        article_title = container.css(".post-block__title a").text.strip
+        author = container.css(".river-byline__authors a").text.strip
+        time_published = container.css(".river-byline time").attribute("datetime").value
+        articles << {title: article_title, time_published: time_published, author: author}
+      end 
     end
-    
     #return the articles Array
-    articles
     
+    #articles
+  
   end
+  binding.pry
+  
+
+   
+    
+ 
     
     # the first article seems to be index 2 in the array.
     # article_title = container.css(".post-block__title a")[2].text.strip
