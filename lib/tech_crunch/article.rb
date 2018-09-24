8a948489eaede11945f77f215547d04f81d88e54
surpreet.mundra <surpreet.mundra@gmail.com>
2018-09-24 17:17:03 +0000

    Created article objects from articles array

--- lib/tech_crunch/article.rb
+++ lib/tech_crunch/article.rb
@@ -7,12 +7,14 @@ class TechCrunch::Article
 
   def self.run
     articles_array = TechCrunch::Scraper.scrape_homepage("https://techcrunch.com/")
+    create_articles_from_homepage_scrape(articles_array)
     binding.pry
+
   end
 
   def self.create_articles_from_homepage_scrape(articles_array)
-    articles_array. each do |article_hash|
-      Article.new(article_hash)
+    articles_array.each do |article_hash|
+      self.new(article_hash)
     end
   end
 
--- lib/tech_crunch/scraper.rb
+++ lib/tech_crunch/scraper.rb
@@ -3,36 +3,35 @@ require 'pry'
 class TechCrunch::Scraper
 
   def items_to_scrape(url)
-    # This method will need to be deleted - only used for checking the html tags to scrape. 
+    # This method will need to be deleted - only used for checking the html tags to scrape.
     doc = Nokogiri::HTML(open(url))
     container = doc.search(".river div").css(".post-block")
     article_title = container.css(".post-block__title a").text.strip
     article_author = container.css(".river-byline__authors a").text.strip
     article_publish_time = container.css(".river-byline time").attribute("datetime").value
   end
-  
+
   def self.scrape_homepage(url)
-    # This method scrapes the homepage and produces and array of articles in the format 
+    # This method scrapes the homepage and produces and array of articles in the format
     # [ {title1, time_published1, author1}, {title2, time_published2, author2} ]
-    
+
     doc = Nokogiri::HTML(open(url))
     container = doc.search(".river div").css(".post-block")
     articles_array = []
-    
+
     container.each do |article|
       article_title = article.css(".post-block__title a").text.strip
       article_author = article.css(".river-byline__authors a").text.strip
       article_publish_time = article.css(".river-byline time").attribute("datetime").value
       articles_array << {title: article_title, time_published: article_publish_time, author: article_author}
-    end 
-    
+    end
+
     articles_array
-    binding.pry
-  end 
-  
-  def self.scrape_article_content
-    # Placeholder to scrape the main article and add this to the associated article hash within the articles_array. 
   end 
 
-#end of class   
-end 
+  def self.scrape_article_content
+    # Placeholder to scrape the main article and add this to the associated article hash within the articles_array.
+  end
+
+#end of class
+end
