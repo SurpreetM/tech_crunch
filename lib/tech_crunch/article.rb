93ece471d7b801dfa882bd5a5303e3fa755552f3
surpreet.mundra <surpreet.mundra@gmail.com>
2018-09-24 16:37:57 +0000

    pulling scraper method to articles class

--- lib/tech_crunch/article.rb
+++ lib/tech_crunch/article.rb
@@ -1,27 +1,31 @@
 require 'pry'
-class TechCrunch::Article 
-  
+class TechCrunch::Article
+
   attr_accessor :title, :time_published, :author
-  
+
   @@all = []
-  
+
+  def self.run
+    articles_array = TechCrunch::Scraper.scrape_homepage("https://techcrunch.com/")
+    binding.pry
+  end
+
   def self.create_articles_from_homepage_scrape(articles_array)
     articles_array. each do |article_hash|
       Article.new(article_hash)
-    end 
-  end 
-  
+    end
+  end
+
   def initialize(article_hash)
     article_hash.each do |attribute, value|
       self.send("#{attribute}=", value)
     end
     @@all << self
-  end 
-  
-  def self.all 
-    @@all 
-  end 
-  
-#end of class  
-end 
-  
+  end
+
+  def self.all
+    @@all
+  end
+
+#end of class
+end
